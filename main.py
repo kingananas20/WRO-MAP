93302f05b18b58907db10d7b90b532b2a02d92e9
kingananas20 <jonasarter@outlook.com>
2024-01-06 14:25:29 +0100

    Everything in 1 file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame, sys
-from calc import getDistanceAndAngle, addPos
-from code import getCode
+from math import sqrt, degrees, pi, atan2
+
 
 pygame.init()
 
@@ -23,6 +23,68 @@ posList = []
 distanceList = []
 angleList = []
 lastPos = None
+
+#Calc functions
+addPos = lambda pos, posList: posList.append(pos)
+
+def calculateDistance(posA, posB):
+    xA, yA = posA
+    xB, yB = posB
+    return round((sqrt((xB - xA) ** 2 + (yB - yA) ** 2) / 1400) * 236.2, 2)
+
+def getDistance(posList):
+    distance = 0
+    for index, pos in enumerate(posList):
+        if len(posList) == index + 1:
+            return distance
+        distance += calculateDistance(posList[index], posList[index + 1])
+
+def calculateAngle(posA, posB, posC):
+    xA, yA = posA
+    xB, yB = posB
+    xC, yC = posC
+
+    if xB - xA == 0:
+        AB = float("inf")
+    else:
+        AB = (yB - yA) / (xB - xA)
+    if xC - xB == 0:
+        return round(degrees(pi / 2))
+    
+    BC = (yC - yB) / (xC - xB)
+
+    return round(degrees(atan2(AB - BC, 1 + AB * BC)), 2)
+
+def getDistanceAndAngle(posList, distanceList, angleList):
+    for index, pos in enumerate(posList):
+        if len(posList) == index + 1:
+            return
+        
+        if index == 0:
+            distanceList.append(calculateDistance(pos, posList[index + 1]))
+            angleList.append(0)
+            continue
+
+        distanceList.append(calculateDistance(pos, posList[index + 1]))
+        angleList.append(calculateAngle(posList[index - 1],pos, posList[index + 1]))
+
+#Code functions
+def getCode(distanceList, angleList):
+    print(f"{distanceList}\n{angleList}")
+    for distance, angle in zip(distanceList, angleList):
+        if -5 < angle < 5:
+            print(f"Strecke({distance})")
+        elif 85 < angle < 95:
+            print(f"Drehung(1, 0, 0, 90)\nStrecke(0, 0, {distance})")
+        elif -95 < angle < -85:
+            print(f"Drehung(1, 0, 0, -90)\nStrecke(0, 0, {distance})")
+        elif 175 < angle < -175:
+            print(f"Strecke(0, 0, -10)")
+            if distance < 10:
+                return
+            print(f"Drehung(1, 0, 0, 180)\nStrecke(0, 0, {distance - 10})")
+        else:
+            print(f"Drehung(1, 0, 0, {angle})\nStrecke(0, 0, {distance})")
 
 #Program
 while True:
